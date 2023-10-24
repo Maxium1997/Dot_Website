79534503a417f360f2cb0e5ea41e97575e58e0a5
DianWei Su <>
2023-10-24 19:32:31 +0800

    Separate category and subject view to different html templates. Added breadcrumb into category and subject html.

--- learning_tree/urls.py
+++ learning_tree/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 
-# from .views import IndexView
+from .views import CategoryView, SubjectView
 
 urlpatterns = [
-    # path('', IndexView.as_view(), name='index'),
+    path('learning_tree/', include([
+        path('<slug:category_slug>', CategoryView.as_view(), name='category'),
+        path('<slug:category_slug>/', include([
+            path('<slug:subject_slug>', SubjectView.as_view(), name='subject'),
+        ])),
+    ])),
 ]
--- learning_tree/views.py
+++ learning_tree/views.py
@@ -1,4 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DeleteView
+
+from .models import Category, Subject
 
 # Create your views here.
 
+
+class CategoryView(ListView):
+    model = Category
+    template_name = 'learning_tree/category.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CategoryView, self).get_context_data(object_list=None, **kwargs)
+        category = get_object_or_404(Category, slug=self.kwargs.get('category_slug'))
+        subjects = Subject.objects.filter(category=category)
+        context['subjects'] = subjects
+        context['categories'] = Category.objects.all()
+        context['s_category'] = Category.objects.get(slug=self.kwargs.get('category_slug'))
+
+        return context
+
+    # def get_queryset(self):
+        # category = get_object_or_404(Category, slug=self.kwargs.get('category_slug'))
+        # return SubCategory.objects.filter(f=category)
+
+
+class SubjectView(DeleteView):
+    model = Subject
+    template_name = 'learning_tree/subject.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(SubjectView, self).get_context_data(object_list=None, **kwargs)
+        return context
+
+    def get_object(self, queryset=None):
+        subject = Subject.objects.get(slug=self.kwargs.get('subject_slug'))
+        return subject
