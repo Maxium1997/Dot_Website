f72e37d272f500fc9f53287b4634ff9f6ee46bc9
DianWei Su <>
2023-12-01 12:02:13 +0800

    Create a incomplete models, Item(main) and sub class Property and equipment.

--- market_place/models.py
+++ market_place/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    serial_number = models.CharField
+    name = models.CharField
+    brand = models.CharField
+    model = models.CharField
+    # use_unit =
+    # manage_unit =
+    # storage_place =
+    # manager =
+
+
+class Property(Item):
+    pass
+
+
+class Equipment(Item):
+    pass
