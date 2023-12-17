b20fa6902284c95762e8a04f8487e9a545ad57a3
DianWei Su <>
2023-12-17 17:46:33 +0800

    Completed the export feature of organization data.

--- Dot_Website/urls.py
+++ Dot_Website/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    path('', include('organization.urls')),
     path('', include('business.urls')),
     path('', include('market_place.urls')),
     path('', include('learning_tree.urls')),
---
+++ organization/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ExportOrganizationToCSV
+
+urlpatterns = [
+    # other patterns...
+    path('export-organization-data/', ExportOrganizationToCSV.as_view(), name='export_organization_data'),
+]
