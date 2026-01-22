f9bd103f47a6111f7e84209809aa60120fc00967
DianWei Su <>
2026-01-23 01:54:49 +0800

    Adjust DATABASES settings

--- Dot_Website/settings.py
+++ Dot_Website/settings.py
@@ -142,9 +142,8 @@ WSGI_APPLICATION = 'Dot_Website.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
-        ssl_require=True if os.getenv('DATABASE_URL') else False
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 
